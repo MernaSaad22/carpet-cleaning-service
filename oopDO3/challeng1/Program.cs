9a57b2cc5f0061368c6f6ccce8a56d7d93e51c2c
Mernasaad21 <mernasaad131@gmail.com>
2025-02-03 22:42:15 +0200

    polymorphism&abxtract

---
+++ oopDO3/challeng1/Program.cs
@@ -0,0 +1,60 @@
+﻿namespace challeng1
+{
+    //polymophrism
+    class Animal
+    {
+        public Animal(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public virtual void AnimalSound()
+        {
+            Console.WriteLine("The animal makes a sound");
+        }
+    }
+    class Pig : Animal
+    {
+        public Pig(string name, int age, double size)
+            : base(name, age)
+        {
+            {
+                Size = size;
+            }
+        }
+        public double Size { get; set; }
+
+
+        public override void AnimalSound()
+        {
+            Console.WriteLine("The pig says: wee wee");
+        }
+    }
+    class Dog : Animal
+    {
+        public Dog(string name, int age) : base(name, age)
+        {
+        }
+
+        public override void AnimalSound()
+        {
+            Console.WriteLine("The dog says: bow wow");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal myAnimal = new Animal("cat",3);     // Create a Animal object
+            Animal myPig = new Pig("pig",5,500);           // Create a Pig object
+            Animal myDog = new Dog("dog",6);           // Create a Dog object
+            myAnimal.AnimalSound(); // The animal makes a sound
+            myPig.AnimalSound();    // The pig says: wee wee
+            myDog.AnimalSound();    // The dog says: bow wow
+        }
+    }
+}
---
+++ oopDO3/challenge2/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace challenge2
+{
+    //abstraction
+    abstract class Employee
+    {
+        protected Employee(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; set; }
+        public abstract double CalculateSalary();
+    }
+    class FullTimeEmployee:Employee
+    {
+        public FullTimeEmployee(string name, double fixedSalary) : base(name)
+        {
+            {
+                this.fixedSalary = fixedSalary;
+            }
+        }
+        public double fixedSalary { get; set; }
+        public override double CalculateSalary()
+        {
+            return fixedSalary;
+        }
+    }
+    class PartTimeEmployee : Employee
+    {
+        public PartTimeEmployee(string name, double hoursWorked, double hourlyRate) : base(name)
+        {
+            {
+                this.hoursWorked = hoursWorked;
+                this.hourlyRate = hourlyRate;
+            }
+        }
+        public double hoursWorked { get; set; }
+        public double hourlyRate { get; set; }
+        public override double CalculateSalary()
+        {
+           return hoursWorked *hourlyRate;
+        }
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee fulltimeemployee = new FullTimeEmployee("omar", 5003);
+            Employee parteimeemployee = new PartTimeEmployee("merna", 23, 100);
+           Console.WriteLine( fulltimeemployee.CalculateSalary());
+             Console.WriteLine( parteimeemployee.CalculateSalary());
+        }
+    }
+}
